daa5c103da701ae72f14d099a66823a150761eb0
Henrique Gomide <henriquepgomide@gmail.com>
2021-01-21 21:31:23 -0300

    add money change script

---
+++ 01_algorithmic_toolbox/week_03/01_money_change.py
@@ -0,0 +1,19 @@
+def money_change(money):
+    assert 0 <= money <= 10 ** 3
+    coins = 0
+
+    while money < 0:
+        if money >= 10:
+            money -= 10
+        elif money >= 5:
+            money -= 5
+        else:
+            money -= 1
+        coins += 1
+
+    return coins
+
+
+if __name__ == "__main__":
+    input_money = int(input())
+    print(money_change(input_money))
